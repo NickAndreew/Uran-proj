0c391107aeeeaf99c9ee1b2fc54f5f60d78bf59e
Nikita Andreiev <nickandreev47@gmail.com>
2018-03-03 22:03:06 +0300

    commit

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -11,7 +11,27 @@ class HomeScreen extends React.Component {
     }
     
     componentDidMount(){
+        window.fbAsyncInit = function() {
+			FB.init({
+			appId      : '424041198008614',
+			cookie     : true,
+			xfbml      : true,
+			version    : 'v2.12'
+			});
+			
+			FB.AppEvents.logPageView();   
+		};
+		
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "https://connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
         window.FB.getLoginStatus(function(response) {
+            console.log(response.status);
             if(response.status=="not_authorized"){
                 console.log(response.status);
             } else {
