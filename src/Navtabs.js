fce54170e0034c260d205030ab7de70bca99cf3f
Nikita Andreiev <nickandreev47@gmail.com>
2018-03-04 00:22:22 +0300

    commit

--- src/Navtabs.js
+++ src/Navtabs.js
@@ -9,6 +9,7 @@ class Navtabs extends React.Component {
 	}
 
 	render() {
+		console.log(this.props);
 		return (
 			<div>
 				<ul className="nav nav-tabs">
@@ -25,7 +26,7 @@ class Navtabs extends React.Component {
 					</div>
 					<div id="menu1" className="tab-pane fade">
 						<h3>Menu 1</h3>
-						<p>{this.props.status}</p>
+						<p>Some content in menu 1.</p>
 					</div>
 					<div id="menu2" className="tab-pane fade">
 						<h3>Menu 2</h3>
