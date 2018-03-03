2a1597794845a6a61b274e0d30148ee7e887e6fe
Nikita Andreiev <nickandreev47@gmail.com>
2018-03-04 00:27:41 +0300

    commit

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './Main.css';
 import HomeScreen from './HomeScreen.js';
 
@@ -8,7 +8,6 @@ class Main extends Component {
 		return (
 			<div className="Main">
 				<HomeScreen />
-				
 			</div>
 		);
 	}
--- src/Navbar.js
+++ src/Navbar.js
@@ -20,7 +20,7 @@ class Navbar extends Component {
 
 	logoutFunction() {
 		window.FB.logout(function(response) {
-			console.log(response)
+			console.log(response);
 		});
 	}
 
@@ -31,18 +31,18 @@ class Navbar extends Component {
 				<nav className="navbar navbar-inverse">
 					<div className="container-fluid">
 						<div className="navbar-header">
-							<a className="navbar-brand" href="#">Facebook Media Viewer</a>
+							<a className="navbar-brand">Facebook Media Viewer</a>
 						</div>
 						<ul className="nav navbar-nav">
-							<li className="active"><a href="#">Home</a></li>
-							<li className="active"><a href="#">Page 1</a></li>
-							<li><a href="#">Page 2</a></li>
+							<li className="active"><a>Home</a></li>
+							<li className="active"><a>Page 1</a></li>
+							<li><a>Page 2</a></li>
 						</ul>
 						<ul className="nav navbar-nav navbar-right">
-							<li><a href="#"><span className="glyphicon glyphicon-user" onClick = {
+							<li><a><span className="glyphicon glyphicon-user" onClick = {
 								this.logoutFunction
 							}></span> Log Out</a></li>
-							<li><a href="#"><span className="glyphicon glyphicon-log-in" onClick = {
+							<li><a><span className="glyphicon glyphicon-log-in" onClick = {
 								this.loginFunction
 							}></span> Login</a></li>
 						</ul>
